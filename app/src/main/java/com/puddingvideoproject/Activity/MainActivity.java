d8e92c9d240d363f41d41c82c49684f2b7405ff2
lenovo <723522164@qq.com>
2015-05-02 13:51:24 +0800

    3

---
+++ app/src/main/java/com/puddingvideoproject/Activity/MainActivity.java
@@ -0,0 +1,20 @@
+package com.puddingvideoproject.Activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+
+import com.puddingvideoproject.R;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//修改
+    }
+
+
+}
